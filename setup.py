a348ced8e68cef145bcb2a63f6d41ea60adfe0f5
Adalfarus <super_sniper_2227@gmx.net>
2023-11-25 16:35:07 +0100

    Forgot to update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='nuisco',
-    version='0.1.0',
+    version='0.2.0',
     description='A faster and more customizable compiler based on nuitka',
     author='Cariel Becker',
     license='GPL-3.0',
